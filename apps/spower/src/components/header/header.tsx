f9a537c1a0e2e8e8a5011269e09d092bc6021c6e
minhdtb <minhdtb.83@gmail.com>
2024-05-06 16:27:58 +0700

    update coding

--- apps/spower/src/components/header/header.tsx
+++ apps/spower/src/components/header/header.tsx
@@ -27,53 +27,53 @@ const Logo = () => (
 export const Header = () => {
   const matches = useRouterState({ select: s => s.matches });
 
-  const breadcrumbs = _.chain(
-    matches.map(match => {
-      const { routeContext } = match;
-      return {
-        title: (routeContext as { title: string }).title,
-        path: match.pathname
-      };
-    })
-  )
-    .groupBy('title')
-    .map(values => {
-      return _.chain(values)
-        .groupBy('path')
-        .map(values => values[0])
-        .value()[0];
-    })
-    .value();
+  // const breadcrumbs = _.chain(
+  //   matches.map(match => {
+  //     const { routeContext } = match;
+  //     return {
+  //       title: (routeContext as { title: string }).title,
+  //       path: match.pathname
+  //     };
+  //   })
+  // )
+  //   .groupBy('title')
+  //   .map(values => {
+  //     return _.chain(values)
+  //       .groupBy('path')
+  //       .map(values => values[0])
+  //       .value()[0];
+  //   })
+  //   .value();
 
   return (
     <div className={'flex w-full justify-between px-2 py-1 shadow'}>
       <div className={'flex items-center justify-center gap-4'}>
         <Logo />
-        <Breadcrumb>
-          <BreadcrumbList>
-            {breadcrumbs.map((it, index) => (
-              <Fragment key={it.path}>
-                <BreadcrumbItem>
-                  <Show
-                    when={index === breadcrumbs.length - 1}
-                    fallback={
-                      <BreadcrumbLink href={it.path}>
-                        <Show when={it.path === '/'} fallback={it.title}>
-                          <LucideHome width={20} height={20} />
-                        </Show>
-                      </BreadcrumbLink>
-                    }
-                  >
-                    <BreadcrumbPage>{it.title}</BreadcrumbPage>
-                  </Show>
-                </BreadcrumbItem>
-                <Show when={index !== breadcrumbs.length - 1}>
-                  <BreadcrumbSeparator />
-                </Show>
-              </Fragment>
-            ))}
-          </BreadcrumbList>
-        </Breadcrumb>
+        {/*<Breadcrumb>*/}
+        {/*  <BreadcrumbList>*/}
+        {/*    {breadcrumbs.map((it, index) => (*/}
+        {/*      <Fragment key={it.path}>*/}
+        {/*        <BreadcrumbItem>*/}
+        {/*          <Show*/}
+        {/*            when={index === breadcrumbs.length - 1}*/}
+        {/*            fallback={*/}
+        {/*              <BreadcrumbLink href={it.path}>*/}
+        {/*                <Show when={it.path === '/'} fallback={it.title}>*/}
+        {/*                  <LucideHome width={20} height={20} />*/}
+        {/*                </Show>*/}
+        {/*              </BreadcrumbLink>*/}
+        {/*            }*/}
+        {/*          >*/}
+        {/*            <BreadcrumbPage>{it.title}</BreadcrumbPage>*/}
+        {/*          </Show>*/}
+        {/*        </BreadcrumbItem>*/}
+        {/*        <Show when={index !== breadcrumbs.length - 1}>*/}
+        {/*          <BreadcrumbSeparator />*/}
+        {/*        </Show>*/}
+        {/*      </Fragment>*/}
+        {/*    ))}*/}
+        {/*  </BreadcrumbList>*/}
+        {/*</Breadcrumb>*/}
       </div>
       <HeaderMenu />
     </div>
