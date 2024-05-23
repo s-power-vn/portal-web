5ef8ffbcb789a6673ad9d05a89020de776470a58
minhdtb <minhdtb.83@gmail.com>
2024-05-23 17:43:34 +0700

    update coding

---
+++ apps/spower/src/components/models/employee/edit-employee-form.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+
+export type EditEmployeeFormProps = {
+  employeeId: string;
+  onSuccess: () => void;
+};
+
+export const EditEmployeeForm: FC<EditEmployeeFormProps> = props => {
+  return null;
+};
---
+++ apps/spower/src/components/models/employee/new-employee-form.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export type NewEmployeeFormProps = {
+  onSuccess: () => void;
+};
+
+export const NewEmployeeForm: FC<NewEmployeeFormProps> = props => {
+  return null;
+};
