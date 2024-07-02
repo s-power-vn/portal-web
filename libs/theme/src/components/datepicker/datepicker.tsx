0bbe027147ba94495f9ae3edd9f8546d6f566dd4
minhdtb <minhdtb.83@gmail.com>
2024-07-02 12:38:08 +0700

    update coding

--- libs/theme/src/components/datepicker/datepicker.tsx
+++ libs/theme/src/components/datepicker/datepicker.tsx
@@ -21,15 +21,16 @@ export type DatePickerProps = {
 
 export const DatePicker: FC<DatePickerProps> = props => {
   const [date, setDate] = useState<DateTime | undefined>(props.value);
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
     <div className={cn('flex items-center gap-2', props.className)}>
-      <Popover>
+      <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
           <Button
             variant={'outline'}
             className={cn(
-              'flex-1 justify-start text-left font-normal ',
+              'relative flex-1 justify-start text-left font-normal',
               !date && 'text-muted-foreground'
             )}
           >
@@ -37,44 +38,48 @@ export const DatePicker: FC<DatePickerProps> = props => {
             <Show when={date} fallback={props.placeholder ?? 'Hãy chọn ngày'}>
               {date?.toFormat('dd/MM/yyyy')}
             </Show>
+            <Show when={!!date}>
+              <Button
+                variant={'outline'}
+                className={cn(
+                  'bg-appError absolute right-2 h-5 w-5 justify-center rounded-full p-0 text-white'
+                )}
+                onClick={e => {
+                  e.stopPropagation();
+                  setDate(undefined);
+                  props.onChange?.(undefined);
+                }}
+              >
+                <Cross2Icon className="h-2 w-2" />
+              </Button>
+            </Show>
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-auto p-0">
+        <PopoverContent className="w-auto p-0" align={'start'}>
           <Calendar
             mode="single"
             selected={date?.toJSDate()}
             onSelect={v => {
               const date = v ? DateTime.fromJSDate(v) : undefined;
               setDate(date);
+              setOpen(false);
               props.onChange?.(date);
             }}
-            initialFocus
           />
+          <Show when={props.showTime}>
+            <div className={'p-2'}>
+              <Timepicker
+                value={date}
+                className={'w-full'}
+                onChange={v => {
+                  setDate(v);
+                  props.onChange?.(v);
+                }}
+              />
+            </div>
+          </Show>
         </PopoverContent>
       </Popover>
-      <Show when={props.showTime}>
-        <Timepicker
-          value={date}
-          onChange={v => {
-            setDate(v);
-            props.onChange?.(v);
-          }}
-        />
-      </Show>
-      <Show when={!!date}>
-        <Button
-          variant={'outline'}
-          className={cn(
-            'text-muted-foreground h-6 w-6 justify-center rounded-full p-0'
-          )}
-          onClick={() => {
-            setDate(undefined);
-            props.onChange?.(undefined);
-          }}
-        >
-          <Cross2Icon className="h-3 w-3" />
-        </Button>
-      </Show>
     </div>
   );
 };
