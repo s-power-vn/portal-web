ad29bb8ec347135b1086e446a5a1b1b7b82764a2
minhdtb <minhdtb.83@gmail.com>
2024-10-10 14:33:06 +0700

    update coding

--- apps/spower/src/routes/__root.tsx
+++ apps/spower/src/routes/__root.tsx
@@ -1,6 +1,16 @@
 import { QueryClient } from '@tanstack/react-query';
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+import { lazy } from 'react';
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null
+    : lazy(() =>
+        import('@tanstack/router-devtools').then(res => ({
+          default: res.TanStackRouterDevtools
+        }))
+      );
 
 export type RouteContext = {
   queryClient?: QueryClient;
