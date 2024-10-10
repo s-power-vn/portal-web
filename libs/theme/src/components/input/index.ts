bce19881b28e891d8f944c23eb63ac0f8e9ebad7
minhdtb <minhdtb.83@gmail.com>
2024-10-10 14:18:37 +0700

    update coding

--- libs/theme/src/components/input/index.ts
+++ libs/theme/src/components/input/index.ts
@@ -1,6 +1,5 @@
 export * from './check-input';
 export * from './debounced-input';
-export * from './integer-input';
 export * from './masked-input';
 export * from './numeric-input';
 export * from './select-input';
--- libs/theme/src/components/input/integer-input.tsx
+++
@@ -1,9 +0,0 @@
-import { InputHTMLAttributes, forwardRef } from 'react';
-
-import { MaskedInput } from './masked-input';
-
-export const IntegerInput = forwardRef(
-  (props: InputHTMLAttributes<HTMLInputElement>, ref) => (
-    <MaskedInput mask={Number} scale={0} autofix={true} ref={ref} {...props} />
-  )
-);
