34302cbb4f719835e2e000e3d08edac591636814
minhdtb <minhdtb.83@gmail.com>
2025-03-19 11:51:00 +0700

    fix: flow property

--- apps/spower/src/components/domains/flow/flow-property.tsx
+++ apps/spower/src/components/domains/flow/flow-property.tsx
@@ -10,6 +10,7 @@ import { Button } from '@minhdtb/storeo-theme';
 
 import type { Flow, FlowType } from '.';
 import { SelectEmployee } from '../employee';
+import { SelectFlowType } from './select-flow-type';
 
 type FlowFormValues = {
   id: string;
@@ -104,14 +105,14 @@ export const FlowProperty: FC<FlowPropertyProps> = ({
   };
 
   return (
-    <div className="flex h-full flex-col">
-      <div className="flex-1 overflow-y-auto">
+    <div className="flex max-h-0 flex-col">
+      <div className="flex-1">
         <div className="space-y-4 p-4">
           {selectedFlow ? (
             <div className="space-y-4">
               <div>
                 <label className="text-sm font-medium">ID</label>
-                <div className="border-input bg-secondary/20 flex h-10 w-full items-center rounded-md border px-3 py-2 text-sm">
+                <div className="border-input bg-appGrayLight flex h-10 w-full items-center rounded-md border px-3 py-2 text-sm">
                   <div className="w-full truncate">{watch('id')}</div>
                 </div>
                 {errors.id && (
@@ -137,17 +138,18 @@ export const FlowProperty: FC<FlowPropertyProps> = ({
               </div>
               <div>
                 <label className="text-sm font-medium">Kiểu đường</label>
-                <select
-                  {...register('type', {
-                    onChange: () => handleSubmit(onSubmit)()
-                  })}
-                  className="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm ring-offset-0 focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50"
-                >
-                  <option value="default">Mặc định</option>
-                  <option value="straight">Thẳng</option>
-                  <option value="step">Bậc thang</option>
-                  <option value="smoothstep">Bậc thang mượt</option>
-                </select>
+                <SelectFlowType
+                  value={watch('type')}
+                  onChange={value => {
+                    setValue('type', value as FlowType, {
+                      shouldDirty: true,
+                      shouldTouch: true,
+                      shouldValidate: true
+                    });
+
+                    handleSubmit(onSubmit)();
+                  }}
+                />
                 {errors.type && (
                   <p className="text-destructive mt-1 text-sm">
                     {errors.type.message}
