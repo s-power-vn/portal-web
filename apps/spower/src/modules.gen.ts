8408cb162abaec6812604a1e1819ecd33c2c0053
minhdtb <minhdtb.83@gmail.com>
2025-03-29 11:57:46 +0700

    fix: fix dynamic form

--- apps/spower/src/modules.gen.ts
+++ apps/spower/src/modules.gen.ts
@@ -7,19 +7,12 @@ const modules = [
       {
         id: 'Request',
         title: 'Yêu cầu mua hàng',
-        description: 'Yêu cầu mua hàng',
-        display: './modules/construction/objects/request/request-display',
-        newForm: './modules/construction/objects/request/form/new-request-form',
-        editForm:
-          './modules/construction/objects/request/form/edit-request-form'
+        description: 'Yêu cầu mua hàng'
       },
       {
         id: 'Price',
         title: 'Bảng giá',
-        description: 'Bảng giá',
-        display: './modules/construction/objects/price/price-display',
-        newForm: './modules/construction/objects/price/form/new-price-form',
-        editForm: './modules/construction/objects/price/form/edit-price-form'
+        description: 'Bảng giá'
       }
     ]
   }
@@ -29,44 +22,40 @@ export function getAllModules() {
   return modules;
 }
 
-export function getObject(objectType: string) {
-  const object = modules
-    .flatMap(m => m.objects)
-    .find(obj => obj.id === objectType);
-  if (!object) {
-    throw new Error(`Object type ${objectType} not found`);
-  }
-  return object;
-}
-
 export function getObjectDisplayComponent(objectType: string) {
-  const path = getObject(objectType).display;
-  return () =>
-    import(
-      /* @vite-ignore */ process.env.NODE_ENV === 'development'
-        ? `/src/${path}`
-        : `./${path}`
-    );
+  switch (objectType) {
+    case 'Request':
+      return () =>
+        import('./modules/construction/objects/request/request-display');
+    case 'Price':
+      return () => import('./modules/construction/objects/price/price-display');
+    default:
+      throw new Error(`Object type ${objectType} not found`);
+  }
 }
 
 export function getObjectNewFormComponent(objectType: string) {
-  const path = getObject(objectType).newForm;
-  return () =>
-    import(
-      /* @vite-ignore */ process.env.NODE_ENV === 'development'
-        ? `/src/${path}`
-        : `./${path}`
-    );
+  switch (objectType) {
+    case 'Request':
+      return () =>
+        import('./modules/construction/objects/request/form/new-request-form');
+    case 'Price':
+      return () =>
+        import('./modules/construction/objects/price/form/new-price-form');
+    default:
+      throw new Error(`Object type ${objectType} not found`);
+  }
 }
 
 export function getObjectEditFormComponent(objectType: string) {
-  const path = getObject(objectType).editForm;
-  return () =>
-    import(
-      /* @vite-ignore */ process.env.NODE_ENV === 'development'
-        ? `/src/${path}`
-        : `./${path}`
-    );
+  switch (objectType) {
+    case 'Request':
+      return () =>
+        import('./modules/construction/objects/request/form/edit-request-form');
+    case 'Price':
+      return () =>
+        import('./modules/construction/objects/price/form/edit-price-form');
+    default:
+      throw new Error(`Object type ${objectType} not found`);
+  }
 }
-
-export default modules;
